e90b62e47c727e68c603f208460add3eae9c271f
nerap <raph.en223@gmail.com>
2021-10-14 18:44:13 +0200

    update(test): Now all the test is working as expected

--- BinanceSymbolFetching/test/basic_config_file_test.py
+++ BinanceSymbolFetching/test/basic_config_file_test.py
@@ -6,34 +6,59 @@ main_name = "BinanceBot"
 
 class TestBasicConfigFile(unittest.TestCase):
 
-    def test_basic_input_08(self):
-        main_stdout = subprocess.run(['python3', main_name, '-s', 'ETGGGG'], stdout=subprocess.PIPE).stdout
-        comp_stdout = str.encode('Symbol is not valid for BinanceBot check your parameters\nInvalid symbol.\nReturn Code : -1121\n')
+    def test_basic_config_file_00(self):
+        main_stdout = subprocess.run(['python3', main_name, 'f', 'file/doesnt/exist'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('python3 ' + main_name + ' -f <file> or --file=<file>\npython3 ' + main_name + ' -h or --help\n')
         self.assertEqual(main_stdout, comp_stdout)
 
-    def test_basic_input_09(self):
-        main_stdout = subprocess.run(['python3', main_name, '-s', 'ETGGGGdfs'], stdout=subprocess.PIPE).stdout
-        comp_stdout = str.encode('Symbol is not valid for BinanceBot check your parameters\nIllegal characters found in parameter \'symbol\'; legal range is \'^[A-Z0-9-_.]{1,20}$\'.\nReturn Code : -1100\n')
+    def test_basic_config_file_01(self):
+        main_stdout = subprocess.run(['python3', main_name, '-f', 'ETGGGGdfs'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: can\'t open ETGGGGdfs\n')
         self.assertEqual(main_stdout, comp_stdout)
     
-    def test_basic_input_10(self):
-        main_stdout = subprocess.run(['python3', main_name, '--symbol=ETGGGGdfs'], stdout=subprocess.PIPE).stdout
-        comp_stdout = str.encode('Symbol is not valid for BinanceBot check your parameters\nIllegal characters found in parameter \'symbol\'; legal range is \'^[A-Z0-9-_.]{1,20}$\'.\nReturn Code : -1100\n')
+    def test_basic_config_file_02(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=ETGGGGdfs'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: can\'t open ETGGGGdfs\n')
         self.assertEqual(main_stdout, comp_stdout)
 
-    def test_basic_input_11(self):
-        main_stdout = subprocess.run(['python3', main_name, '--symbol=', 'ETGGGGdfs'], stdout=subprocess.PIPE).stdout
-        comp_stdout = str.encode('Symbol is not valid for BinanceBot check your parameters\nParameter \'symbol\' was empty.\nReturn Code : -1105\n')
+    def test_basic_config_file_03(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=', 'ETGGGGdfs'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: configuration file missing\n')
         self.assertEqual(main_stdout, comp_stdout)
 
-    def test_basic_input_12(self):
-        main_stdout = subprocess.run(['python3', main_name, '--symbol=LINKBTC'], stdout=subprocess.PIPE).stdout
-        comp_stdout = str.encode('LINKBTC\n')
+    def test_basic_config_file_04(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_valid'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('[\'BTCUSDT\', \'LINKBTC\', \'BTCBUSD\']\n')
         self.assertEqual(main_stdout, comp_stdout)
     
-    def test_basic_input_13(self):
-        main_stdout = subprocess.run(['python3', main_name, '-s', 'LINKBTC'], stdout=subprocess.PIPE).stdout
-        comp_stdout = str.encode('LINKBTC\n')
+    def test_basic_config_file_05(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_duplicate'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: duplicated symbol BTCUSDT in configuration file\n')
+        self.assertEqual(main_stdout, comp_stdout)
+
+    def test_basic_config_file_06(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_empty'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('test/test_config_file/config_file_empty is not a valid config file\n')
+        self.assertEqual(main_stdout, comp_stdout)
+
+    def test_basic_config_file_07(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_empty_symbols'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: your configuration file is empty\n')
+        self.assertEqual(main_stdout, comp_stdout)
+    
+    def test_basic_config_file_08(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_json_empty'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: configuration file doesn\'t have the right key\n')
+        self.assertEqual(main_stdout, comp_stdout)
+
+    def test_basic_config_file_09(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_wrong_format'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('test/test_config_file/config_file_wrong_format is not a valid config file\n')
+        self.assertEqual(main_stdout, comp_stdout)
+
+    def test_basic_config_file_09(self):
+        main_stdout = subprocess.run(['python3', main_name, '--file=test/test_config_file/config_file_wrong_key'], stdout=subprocess.PIPE).stdout
+        comp_stdout = str.encode('Error: configuration file doesn\'t have the right key\n')
         self.assertEqual(main_stdout, comp_stdout)
 
 if __name__ == '__main__':
