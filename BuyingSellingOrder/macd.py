84c59ff6cb94efcc6a595a8bb10b298e1fec73fd
nerap <raph.en223@gmai.com>
2021-10-16 12:18:23 +0200

    fix(Trying): testing porfit on 4 symbol

--- BuyingSellingOrder/macd.py
+++ BuyingSellingOrder/macd.py
@@ -6,6 +6,7 @@ import pprint
 import numpy as np
 import matplotlib.pyplot as plt
 import requests as rq
+import _thread
 import ta
 from time import sleep
 from binance.exceptions import BinanceAPIException
@@ -17,32 +18,14 @@ def get_data_frame(client, symbol, interval, lookback):
     except BinanceAPIException as e:
         print(e)
         sleep(60)
-        df = pd.DataFrame(client.get_historical_klines(symbol, interval, starttime))
+        df = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback + ' min ago UTC'))
+    
     df = df.iloc[:, :6]
     df.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
     df = df.set_index('Time')
     df.index = pd.to_datetime(df.index, unit="ms")
-    return df.astype(float)
-
-def macd(symbol, qty, client, open_position=False):
-    while True:
-        df = get_data_frame(client, symbol)
-        if not open_position:
-            if ta.trend.macd_diff(df.Close).iloc[-1] > 0 and ta.trend.macd_diff(df.Close).iloc[-2] < 0:
-                buy_price = int(client.get_symbol_ticker(symbol=symbol)) * qty
-                print("Buy at : " + str(buy_price))
-                open_position = True
-                break
-    if open_position:
-        while True:
-            df = get_data_frame(client, symbol)
-            if ta.trend.macd_diff(df.Close).iloc[-1] < 0 and ta.trend.macd_diff(df.Close).iloc[-2] > 0:
-                sell_price = int(client.get_symbol_ticker(symbol=symbol)) * qty
-                print("Selling at : " + str(sell_price))
-                open_position = False
-                break
-    print(f'profit = {(sell_price - buy_price)/buy_price}')
-        
+    df = df.astype(float)
+    return df
 
 
 def applytechnicals(df):
@@ -51,8 +34,6 @@ def applytechnicals(df):
     df['rsi'] = ta.momentum.rsi(df.Close, window=14)
     df['macd'] = ta.trend.macd_diff(df.Close)
     df.dropna(inplace=True)
-
-
 
 class Signals:
         def __init__(self, df, lags):
@@ -68,7 +49,11 @@ class Signals:
 
         def decide(self):
             self.df['trigger'] = np.where(self.gettriger(), 1, 0)
-            self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20, 80)) & (self.df.rsi > 50) & (self.df.macd > 0), 1, 0)
+            self.df['Buy'] =    np.where((self.df.trigger) &
+                                (self.df['%K'].between(20,80)) & 
+                                (self.df['%D'].between(20, 80)) & 
+                                (self.df.rsi > 50) & 
+                                (self.df.macd > 0), 1, 0)
 
 
 
@@ -77,32 +62,54 @@ def strategy(symbol, qty, client, open_position=False):
     applytechnicals(df)
     inst = Signals(df, 25)
     inst.decide()
-    file_object = open('sample.txt', 'a')
+    file_object = open(symbol, 'a')
     print(f'current Close is ' + str(df.Close.iloc[-1]))
     if (df.Buy.iloc[-1]):
-        buy_price = float(client.get_symbol_ticker(symbol=symbol)['price']) * qty
+        buy_price = df.Close[-1]
         print("Buy at : " + str(buy_price))
         open_position = True
     while open_position:
         time.sleep(0.5)
         df = get_data_frame(client, symbol, "1m", '2')
         print(f'current Close is ' + str(df.Close.iloc[-1]))
-        print(f'current Target is ' + str(buy_price * 1.0005))
+        print(f'current Close Second is ' + str(df.Close[-1]))
+        print(f'current Target is ' + str(buy_price * 1.005))
         print(f'current Stop is ' + str(buy_price * 0.995))
-        if df.Close[-1] <= buy_price * 0.995 or df.Close[-1] >= 1.005 * buy_price:
-            sell_price = float(client.get_symbol_ticker(symbol=symbol)['price']) * qty
+        if df.Close[-1] <= (buy_price * 0.995) or df.Close[-1] >= (buy_price * 1.005):
+            sell_price = df.Close[-1]
             print("sell at : " + str(sell_price))
-            file_object.write(str(df.Time[-1]) + " Profit :" + str((sell_price - buy_price)) * "\n")
+            file_object.write(str(str((sell_price - buy_price) * qty) +  "\n"))
             file_object.close()
+            sleep(60)
             break
+
+
+
+def thread(symbol, qty, client):
+    while True:
+        strategy(symbol, qty, client)
+        sleep(0.5)
+
 
 def main():
     api_key = os.environ.get('api_key')
     secret_api_key = os.environ.get('secret_api_key')
     symbol = "BTCUSDT"
+    symbol2 = "ETHUSDT"
+    symbol3 = "XRPUSDT"
+    symbol4 = "DOGEUSDT"
     client = Client(api_key, secret_api_key)
+    #df = get_data_frame(client, symbol, "1m", '100')
+    #applytechnicals(df)
+    #inst = Signals(df, 5)
+    #inst.decide()
+    #print(df[df.Buy == 1])
+    
+    _thread.start_new_thread( thread, (symbol, 10, client, ))
+    _thread.start_new_thread( thread, (symbol2, 10, client, ))
+    _thread.start_new_thread( thread, (symbol3, 10, client, ))
+    _thread.start_new_thread( thread, (symbol4, 10, client, ))
     while True:
-        strategy(symbol, 0.1, client)
-        #sleep(0.5)
+        pass
     #print(df)
     
