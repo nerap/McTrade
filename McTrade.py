3152743ee6377b44074f1410635827903d5848c8
nerap <raph.en223@gmail.com>
2021-10-19 16:29:35 +0200

    doc(most of the file): documentation

--- McTrade.py
+++ McTrade.py
@@ -7,19 +7,31 @@ from dotenv import load_dotenv
 from srcs.entry_parsing import configuration_file_parsing as cfp
 from binance.client import Client
 
+# Loading environnement variable
+# Parsing the configuration file
+# Launching McTrade to trade each symbol
 
 if __name__ == "__main__":
+
+    # Loading .env file with all environnement variable
+
     load_dotenv()
     if os.environ.get('api_key') == None or os.environ.get('secret_api_key') == None:
         print ("Error: no api_key found, pls create .env file at the root of the main direction, with api_key and secret_api_key variable")
         sys.exit(1)
-    symbols = cfp.parse_entry(sys.argv[1:])
 
-    mctrade = McTrade(symbols)
+    # Parsing the argument to retrive the configuration file 
+
+    config = cfp.parse_entry(sys.argv[1:])
+    
+    # Creating McTrade passing the config object as argument
+
+    mctrade = McTrade(config)
+
+    # Starting each thread to trade symbol requested in the config file
+
     mctrade.starting_symbol_order()
+    
+    # Main loop checking if all thread are alive
+    
     mctrade.main_loop()
-    print(mctrade)
-    #s = symb.Symbol(symbols[0], client)
-    #print(s.coin_floor_quantity)
-    #print(s.quote_floor_quantity)
-    #mac.starting_loop_order(symbols)
--- srcs/fetching_symbols/fetching_symbols.py
+++ srcs/fetching_symbols/fetching_symbols.py
@@ -2,18 +2,41 @@
 
 import pandas as pd
 
-# Reformate the json response "res" to a pandas DataFrame 
+# Getting the historical data of a certain symbol overtime
+# @Return : the data in pandas.DataFrame format
 
 def get_data_frame(client, symbol, interval, lookback):
+
+    # Convert all the data from BinanceAPI to DataFrame format
+    # Symbol : -> "BTCUSDT", "ETHUSDT", "COMPBUSD", etc..
+    # Interval : Time spent between each data such as -> "1m", "30m", "1h", etc..
+    # Lookback : How far you want to get the data, "100m", "3 day ago UTC", "1w", etc..
+
     try:
+
         data_frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback))
+
     except BinanceAPIException as e:
+    
+    # After a certain time the client might timeout, so we catch it and restart after 60 seconds
+
         print(e)
         sleep(60)
         data_frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback))
+
+    # Remove the 6 last columns considered useless to McTrade and waste of time to keep them
+    
     data_frame = data_frame.iloc[:, :6]
+
     data_frame.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+    
+    # Convert timestamp column to datetime format, for readable purpose
+    
     data_frame = data_frame.set_index('Time')
     data_frame.index = pd.to_datetime(data_frame.index, unit="ms")
+
+    # Value in the DataFrame should be in float format for obvious reason
+
     data_frame = data_frame.astype(float)
+
     return data_frame
